5440e7249519ac17a719d7d68db6d3455d37414b
RenarsStruve <133567126+RenarsStruve@users.noreply.github.com>
2023-05-14 17:18:21 +0300

    3_SELECT.sql

--- 3_SELECT.sql
+++ 3_SELECT.sql
@@ -1,3 +1,4 @@
+
 SELECT COUNT(*) FROM [User];
 
 --This statement selects the count of all rows in the "User" table.
@@ -47,9 +48,9 @@ SELECT s.Name
 FROM Staff s
 JOIN Role r ON s.ID = r.StaffID
 JOIN Content c ON r.ContentID = c.ID
-WHERE c.[Year] = 2020;
+WHERE c.[Year] = 2008;
 
---List all the staff members who have worked on content released in 2020
+--List all the staff members who have worked on content released in 2008
 
 
 
@@ -58,19 +59,9 @@ SELECT s.Name
 FROM Staff s
 JOIN Role r ON s.ID = r.StaffID
 JOIN Rating ra ON r.ContentID = ra.ContentID
-WHERE ra.Score >= 8;
+WHERE ra.Score >= 4;
 
---List all the staff members who have worked on content with a score of 8 or higher
-
-
-
-
-SELECT u.Name
-FROM [User] u
-JOIN Rating r ON u.ID = r.UserID
-WHERE r.Review IS NOT NULL;
-
---List all the users who have reviewed a content
+--List all the staff members who have worked on content with a score of 4 or higher
 
 
 
@@ -78,19 +69,18 @@ SELECT c.Title, s.Name, ro.Role
 FROM [Content] c
 JOIN Role ro ON ro.ContentID = c.ID
 JOIN Staff s ON s.ID = ro.StaffID
-WHERE c.Title = 'Breaking Bad';
+WHERE c.Title = 'Thor';
 
---This select statement retrieves content of 'Breaking Bad', the names of all the staff members who had a role and the roles they had.
-
-
+--This select statement retrieves content of 'Thor', the names of all the staff members who had a role and the roles they had.
 
 
-SELECT f.Name
-FROM Franchise f
-INNER JOIN Content c ON f.ID = c.FranchiseID
-WHERE c.EpisodeCount > 50;
 
---List all the franchises that have content with more than 50 episodes.
+
+SELECT *
+FROM [Content]
+WHERE EpisodeCount > 10;
+
+--outputs all of the content that has more than 10 episodes.
 
 
 
@@ -124,9 +114,9 @@ WHERE [User].[Name] = 'John Doe';
 SELECT c.Title, r.Score
 FROM Content c
 INNER JOIN Rating r ON c.ID = r.ContentID
-WHERE r.Score > 9;
+WHERE r.Score > 4;
 
--- selects content that is rated higher than 9
+-- selects content that is rated higher than 4
 
 
 
@@ -134,7 +124,100 @@ SELECT ch.Name, AVG(r.Score) AS AvgScore
 FROM Character ch
 INNER JOIN ContentCharacter cc ON ch.ID = cc.CharacterID
 INNER JOIN Rating r ON cc.ContentID = r.ContentID
-GROUP BY ch.ID
+GROUP BY ch.ID, ch.Name
 ORDER BY AvgScore DESC
 
---returns characters by rating
+--returns characters sorted by best overall ratings
+
+
+SELECT g.Genre, c.Title
+FROM (
+    SELECT cg.GenreID, COUNT(*) AS Count
+    FROM ContentGenre cg
+    GROUP BY cg.GenreID
+) AS top_genre
+INNER JOIN Genre g ON top_genre.GenreID = g.ID
+INNER JOIN ContentGenre cg ON top_genre.GenreID = cg.GenreID
+INNER JOIN Content c ON cg.ContentID = c.ID
+ORDER BY top_genre.Count DESC;
+
+--returns genres with most contents and the content
+
+
+
+SELECT Name 
+FROM Studio 
+WHERE ID IN 
+    (SELECT StudioID 
+     FROM ContentStudio 
+     WHERE ContentID IN 
+         (SELECT ID 
+          FROM [Content] 
+          WHERE ID IN 
+              (SELECT ContentID 
+               FROM ContentGenre 
+               WHERE GenreID = 
+                   (SELECT ID 
+                    FROM Genre 
+                    WHERE Genre = 'Action'))));
+
+--outputs the names of all studios that have produced content in the "Action" genre:
+
+
+SELECT TOP 10 u.Name, COUNT(*) AS ReviewCount
+FROM [User] u
+INNER JOIN [Rating] r ON u.ID = r.UserID
+GROUP BY u.ID, u.Name
+ORDER BY ReviewCount DESC
+
+--outputs most active users
+
+
+SELECT Name 
+FROM [User] 
+WHERE ID IN 
+    (SELECT UserID 
+     FROM [Rating]);
+
+--outputs the names of all users who have rated at least one content:
+
+
+
+SELECT Rating.Score, Rating.Progress, Rating.StartDate, Rating.FinishDate, Rating.Review, Content.Title
+FROM Rating
+JOIN [User] ON Rating.UserID = [User].ID
+JOIN Content ON Rating.ContentID = Content.ID
+WHERE [User].[Name] = 'John Doe'
+
+
+-- outputs all the ratings for a specific user
+
+
+
+SELECT [Character].Name
+FROM FavoriteCharacter
+JOIN [User] ON FavoriteCharacter.UserID = [User].ID
+JOIN [Character] ON FavoriteCharacter.CharacterID = [Character].ID
+WHERE [User].[Name] = 'Jane Smith'
+
+
+-- outputs all the favorite characters for a specific user
+
+
+
+SELECT Staff.Name
+FROM FavoriteStaff
+JOIN [User] ON FavoriteStaff.UserID = [User].ID
+JOIN Staff ON FavoriteStaff.StaffID = Staff.ID
+WHERE [User].[Name] = 'Daniel Wilson'
+
+-- outputs all the favorite staff for a specific user
+
+
+
+SELECT COUNT(*) AS NumRatings
+FROM [Rating]
+WHERE ContentID = 4;
+
+--Retrieve the number of ratings that a specific content has received.
+
